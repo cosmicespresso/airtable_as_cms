a74765ff50d81f57a625c2f8da03e81da854a2b4
Kallirroi <kallirroi.retzepi@gmail.com>
2020-06-08 12:10:40 -0400

    rendering content agnostically

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## Airtable as CMS
 
-Boilerplate code for a frontend that uses Airtable as a CMS. Built using `create-react-app`.  
+Boilerplate code for a frontend that uses [Airtable](https://airtable.com/) as a CMS. Built using `create-react-app`.  
 
+Read more about the Airtable API [here](https://airtable.com/api).
 
 ## How to run locally
 
@@ -17,3 +18,7 @@ REACT_APP_AIRTABLE_BASE=YOUR_AIRTABLE_BASE_ID
 REACT_APP_AIRTABLE_TABLE=YOUR_AIRTABLE_TAB_NAME
 ```
 
+If you change the details above, you need to restart your server.
+
+## How to deploy
+I am using [Vercel](https://vercel.com/) (ex-Zeit) or [Netlify](https://app.netlify.com/). Don't forget to set up the `env` variables (both services offer a way to do that on their platform GUI).
--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import dotenv from 'dotenv'; 
+
 import './App.css';
 
+dotenv.config()
+
+const process_api_key = process.env.REACT_APP_AIRTABLE_API_KEY;
+const airtable_api_url = process.env.REACT_APP_AIRTABLE_API_URL;
+const airtable_table = process.env.REACT_APP_AIRTABLE_TABLE;
+const airtable_base = process.env.REACT_APP_AIRTABLE_BASE; 
+
 function App() {
+  
+  const [airtableData, setAirtableData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=>{
+    fetch(`${airtable_api_url}/${airtable_base}/${airtable_table}`, {
+       "headers": {"Authorization": `Bearer ${process_api_key}`}
+    })
+    .then(res => res.json())
+    .then((data) => {
+      setAirtableData(data.records)
+    })
+    setIsLoading(false);
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Entries</header>
+
+      <div className="entries">
+        {isLoading && <div>Loading....</div>}
+
+        {airtableData !== undefined && airtableData.map( (entry, i) => {
+          const fields = entry.fields;
+          return (
+            <div className='entry' key={i}>
+              {Object.keys(fields).map( (fieldKey, j) => {
+                return (
+                  <div className='field left' key={j}>{fields[fieldKey]}</div>
+                )
+              })}
+            </div>
+          )
+         })
+        }  
+      </div>
     </div>
   );
 }
